efaa4533b626ddfde0af6654133d64752f473bc8
Phil-Menard <phil.menard1668@gmail.com>
2024-11-06 18:12:56 +0100

    added strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,5 +12,26 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	
+	size_t	len_dst;
+	size_t	len_src;
+
+	len_dst = 0;
+	while (len_dst < size && dst[len_dst])
+		len_dst++;
+	len_src = 0;
+	while (src[len_src])
+		len_src++;
+	if (size == len_dst)
+		return (size + len_src);
+	dst += len_dst;
+	size -= len_dst;
+	while (*src && size > 1)
+	{
+		*dst = *src;
+		dst++;
+		src++;
+		size--;
+	}
+	*dst = '\0';
+	return (len_dst + len_src);
 }
