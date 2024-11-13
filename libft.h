0377968a8cedd666aa4683aa1ee8ef13f1a05492
Philippe Menard <pmenard@c1r3p7.42perpignan.fr>
2024-11-13 16:04:39 +0100

    finished libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pmenard <pmenard@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 11:35:59 by pmenard           #+#    #+#             */
-/*   Updated: 2024/11/13 14:59:21 by pmenard          ###   ########.fr       */
+/*   Updated: 2024/11/13 15:45:05 by pmenard          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,5 +45,9 @@ char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char *));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 #endif
